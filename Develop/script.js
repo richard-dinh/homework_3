aec63c69ea603798579b3675ff68899717ac88f7
richard-dinh <richarddinh@gmail.com>
2020-01-22 19:48:14 -0800

    added prompts for password specification

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,33 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+console.log(`GenerateBtn is ${generateBtn}`)
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+function writePassword() {
+
+  let passLength = prompt('How long do you want the password to be (8 - 128 characters)?')
+  //Keeps running into an integer is passed through and is between 8 and 128
+  while(!Number.isInteger(parseInt(passLength,10)) || parseInt(passLength, 10)<8 || parseInt(passLength,10)>128){
+    passLength = prompt('Invalid Input! Enter a number from 8-128.')
+  }
+  console.log(passLength)
+  let specialCharacters = prompt('What type of characters do you want (uppercase, lowercase, numeric, or special characters)?')
+
+  //loop to ensure right character is entered
+  while (specialCharacters !== 'uppercase' && specialCharacters !== 'lowercase' && specialCharacters !== 'numeric' && specialCharacters !== 'special characters'){
+    specialCharacters = prompt('Invalid Input! Please enter either uppercase, lowercase, numeric, or special character.')
+  }
+  //var password = generatePassword();
+
+  //this line selects the element with the password ID
+  var passwordText = document.querySelector("#password")
+
+  // This line is the password displayed in the box after the button is clicked
+  // passwordText.value = password;
+  passwordText.value = password
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
